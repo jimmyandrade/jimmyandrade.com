1e762298f701962c2b09b64a44ad71cfd4acfb5c
Jimmy Andrade <contato@jimmyandrade.com>
2024-09-11 17:13:35 -0300

    feat: add redirect for links url with emoji

--- next.config.js
+++ next.config.js
@@ -25,6 +25,11 @@ const nextConfig = {
   redirects: async () => {
     return [
       {
+        source: '/%F0%9F%94%97',
+        destination: '/links',
+        permanent: true,
+      },
+      {
         source: '/manifest.json',
         destination: '/manifest.webmanifest',
         permanent: true,
